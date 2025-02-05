a8c4476ac898f45a97c70f87e20c5e9e0e52d9e1
Shaayer <qalam0720@gmail.com>
2025-02-04 20:24:11 -0800

    Initial Commit

---
+++ ChatProgram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChatProgram
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Hello World!");
+        }
+    }
+}
