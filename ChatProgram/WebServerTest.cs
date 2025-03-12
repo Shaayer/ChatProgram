e63310f59e5864508e7bb4151bc1cf526125af4a
Shaayer <qalam0720@gmail.com>
2025-03-11 20:17:01 -0700

    Working server connection between server and user(terminal)

--- ChatProgram/WebServerTest.cs
+++ ChatProgram/WebServerTest.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
+using System.Runtime.Remoting.Lifetime;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -9,7 +10,24 @@ using System.Threading.Tasks;
 namespace MrV {
   class WebserverTest {
     public static void Main(string[] args) {
-      IPEndPoint endpoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8080);
+      string input=Console.ReadLine();
+      if (input == "s")
+      {
+        Server();
+      }
+      else
+      {
+        Client();
+      }
+      
+    }
+
+    public static string connectionIP = "10.1.1.100";
+    public static int connectionPort = 8080;
+    public static void Server()
+    {
+      List<NetworkStream> streams = new List<NetworkStream>();
+      IPEndPoint endpoint = new IPEndPoint(IPAddress.Parse(connectionIP), connectionPort);
       string timestampServerStart = GetHttpTimeStampString(DateTime.UtcNow);
       TcpListener listener = new TcpListener(endpoint);
       listener.Start();
@@ -20,10 +38,12 @@ namespace MrV {
         while (!clientSocketTask.IsCompleted) {
           Log($"waiting ... {(DateTime.UtcNow - waitStart).Seconds}\r", ConsoleColor.Yellow);
           Thread.Sleep(1);
+          ServiceStreams(streams);
           if (UserWantsToQuit()) { return; }
         }
         LogLine("\nclient connected: " + clientSocketTask.Result.Client.RemoteEndPoint, ConsoleColor.Green);
         NetworkStream clientStream = clientSocketTask.Result.GetStream();
+        streams.Add(clientStream);
         int bytesReceived = 0;
         List<string> inputChunks = new List<string>();
         while (clientStream.DataAvailable) {
@@ -38,7 +58,72 @@ namespace MrV {
         LogLine($"\nreceived {bytesReceived} bytes:", ConsoleColor.Green);
         Console.Write(string.Join("", inputChunks));
         SendHtmlResponse(clientStream, "<b>hello</b> world!", timestampServerStart);
-        clientStream.Close();
+        // clientStream.Close();
+      }
+    }
+
+    public static void ServiceStreams(List<NetworkStream> streams)
+    {
+      string inputData = "";
+      byte[] bytes = new byte[1024];
+      for (int i = 0; i < streams.Count; i++)
+      {
+        NetworkStream s = streams[i];
+        try
+        {
+          if (s.DataAvailable)
+          {
+            int bytesReceived = s.Read(bytes, 0, bytes.Length);
+            inputData += Encoding.ASCII.GetString(bytes, 0, bytesReceived);
+          }
+        }
+        catch (Exception e)
+        {
+          Console.WriteLine(e.Message);
+        }
+      }
+
+      if (inputData.Length > 0)
+      {
+        Console.WriteLine(inputData);
+      }
+      for (int i = 0; i < streams.Count; i++)
+      {
+        NetworkStream s = streams[i];
+        try
+        {
+          s.Write(Encoding.ASCII.GetBytes(inputData), 0, inputData.Length);
+        }
+        catch(Exception e)
+        {
+          Console.WriteLine(e.Message);
+        }
+      }
+    }
+    public static void Client()
+    {
+      TcpClient client = new TcpClient();
+      client.Connect(connectionIP, connectionPort);
+      NetworkStream clientStream = client.GetStream();
+      byte[] bytes = Encoding.ASCII.GetBytes("<b>hello</b>");
+      clientStream.Write(bytes, 0, bytes.Length);
+      clientStream.Flush();
+      while (!UserWantsToQuit())
+      {
+        if (clientStream.DataAvailable)
+        {
+          int bytesReceived = clientStream.Read(bytes, 0, bytes.Length);
+          Console.WriteLine($"Received ({bytesReceived} bytes) " +
+                            $"{Encoding.ASCII.GetString(bytes, 0, bytesReceived)}");
+        }
+
+        if (Console.KeyAvailable)
+        {
+          string inputText = Console.ReadLine();
+          byte[] Bytes = Encoding.ASCII.GetBytes(inputText);
+          clientStream.Write(Bytes, 0, Bytes.Length);
+          clientStream.Flush();
+        }
       }
     }
 
